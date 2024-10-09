6932b202eb3b68dfb4beba746e4a627fe1b15566
冯立 <49088278+pleasetakeup@users.noreply.github.com>
2024-10-09 16:35:33 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,12 @@ BaudRate.BaudRate115200
 )
 DFRobot_GS01.begin()
 basic.forever(function () {
-    DFRobot_GS01.getFaceNumber()
-// serial.writeString("" + (DFRobot_GS01.getFaceNumber()))
-    basic.pause(1000)
+    // serial.writeString("" + (DFRobot_GS01.getFaceNumber()))
+    basic.pause(2000)
     if (DFRobot_GS01.getFaceNumber() > 0) {
         basic.pause(100)
-        serial.writeNumber(DFRobot_GS01.getGestureType())
+        serial.writeString("TYPE:" + DFRobot_GS01.getGestureType())
+        serial.writeString("X:" + DFRobot_GS01.getFaceLocationX())
+        serial.writeString("Y:" + DFRobot_GS01.getFaceLocationY())
     }
 })
